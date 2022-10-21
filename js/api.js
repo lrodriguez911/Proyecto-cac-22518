4af1717239a6194d0bf923cf77ae3ec45972ed92
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-10-20 21:41:09 -0300

    nueva caracteristica pagina personajes

--- js/api.js
+++ js/api.js
@@ -1,23 +1,35 @@
-const app = document.querySelector('api')
-const api = document.getElementById('api')
+/* Informacion de API de personajes mas relevantes URL */
 const URL = 'https://swapi.dev/api/people/'
-const valueInput = document.getElementById('valueInput')
-const INPUT = document.getElementById('GET-name')
 
-console.log(INPUT)
-try {
-    fetch(URL)
+/* Selectores de los elementos de la pagina */
+const api = document.getElementById('api')
+const buttonLuke = document.getElementById('buttonLuke')
+const valueInput = document.getElementById('valueInput')
+
+
+console.log(buttonLuke.value)
+const recibeInfo = async (filtro) => {
+    try {
+     fetch(URL, {cache: 'no-cache'})
 .then(e => e.json())
-/* .then(data => valueInput.innerHTML = JSON.stringify(data)) */
-.then((data) => {
-     let resData = data.results.map(e => e.name)
-    /* data.map(e => valueInput.innerHTML(<li>{e.name}</li>)) */
-    valueInput.innerHTML = resData 
+.then((data) => {   
+        console.log(filtro)
+     const dataFilter = data.results.filter(e => e.name === filtro)
+     api.innerHTML = `<div class="card">
+     <img
+       src="/images/Personajes/${filtro.replace(' ', '-')}.jpg"
+       alt="star wars battlefront"
+     />
+     <h4>${JSON.stringify(dataFilter)}</h4>
+    
+   </div>`
+   
 }
  )
 } catch (error) {
-    
+    console.log(error)
+}
 }
 
-
+buttonLuke.addEventListener('click', function(){recibeInfo(this.value)})
 /* algo */
