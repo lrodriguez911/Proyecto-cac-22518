158ef84e81c50ccb573f9800fb8b97c9c971e8d5
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-10-22 22:16:06 -0300

    agregado pagina contacto

--- js/api.js
+++ js/api.js
@@ -3,33 +3,38 @@ const URL = 'https://swapi.dev/api/people/'
 
 /* Selectores de los elementos de la pagina */
 const api = document.getElementById('api')
-const buttonLuke = document.getElementById('buttonLuke')
-const valueInput = document.getElementById('valueInput')
+const contenedorCard = document.getElementById('contenedor-card')
+const cards = document.querySelectorAll('.card')
 
 
-console.log(buttonLuke.value)
-const recibeInfo = async (filtro) => {
+console.log(cards)
+
+const recibeInfo = async () => {
     try {
-     fetch(URL, {cache: 'no-cache'})
+fetch(URL, {cache: 'no-cache'})
 .then(e => e.json())
 .then((data) => {   
-        console.log(filtro)
-     const dataFilter = data.results.filter(e => e.name === filtro)
-     api.innerHTML = `<div class="card">
-     <img
-       src="/images/Personajes/${filtro.replace(' ', '-')}.jpg"
-       alt="star wars battlefront"
-     />
-     <h4>${JSON.stringify(dataFilter)}</h4>
-    
-   </div>`
-   
+    console.log(data.results)
+     const tpl = data.results.map(e => {
+      return `<div class="card">
+      <img
+        src="/images/Personajes/${e.name.replace(' ', '').replace('-', '')}.png"
+        alt="star wars battlefront"
+      />
+      <h4>${JSON.stringify(e.name)}</h4>
+      <h5>Altura:${JSON.stringify(e.height)}cm</h5>
+      <h5>Peso:${JSON.stringify(e.mass)}kg</h5>
+    </div>`  }
+  ).join('')
+    contenedorCard.innerHTML =  tpl
 }
- )
+)
 } catch (error) {
     console.log(error)
 }
 }
-
-buttonLuke.addEventListener('click', function(){recibeInfo(this.value)})
+window.addEventListener('load', () => recibeInfo())
+/* buttonLuke.addEventListener('click', function(){recibeInfo(this.value)}) */
 /* algo */
+
+/* */
