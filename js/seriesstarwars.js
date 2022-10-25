9d5579c797ef883eb0e0c73d8be793829d46eb5a
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-10-25 16:01:22 -0300

    add validar formulario

--- js/seriesstarwars.js
+++ js/seriesstarwars.js
@@ -1,25 +1,15 @@
 /* CAMBIO DE ICONO SONIDO */
+
 function togglePlay(video) {
-    var audio = document.getElementsByTagName("audio")[0];
-function togglePlay() {
     let audio = document.getElementsByTagName("audio")[0];
     if (audio) {
       if (audio.paused) {
           audio.play();
-          document.getElementById("button").src = "/images/sound2.ico";
+          document.getElementById("button").src = "../images/sound2.ico";
       } else {
           audio.pause();
-          document.getElementById("button").src = "/images/muted.ico";
+          document.getElementById("button").src = "../images/muted.ico";
       }
     }
   }
 
-
-/* Barra hamburguesa */
-/* Barra */
-  const nav = document.querySelector('nav')
-
-  document.querySelector('#burger').addEventListener('click',(e) => {
-    e.currentTarget.classList.toggle("active")
-    nav.classList.toggle('show')
-  })
